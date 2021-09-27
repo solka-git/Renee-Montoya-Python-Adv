d4b78dcc59842021ac7f9b9b09ba067f0e9b4226
lluzhnyi <liubomyr@cozymeal.com>
2021-09-27 21:16:56 +0300

    Tests2 lecture

---
+++ lectures/tests2/ReneeMontoyaApp/test_plant.py
@@ -0,0 +1,12 @@
+from models.plant import Plant
+from fixtures import *
+
+
+def test_plant_save(plant):
+    file = open('database/tests/test.json', 'w')
+    file.write('[]')
+    file.close()
+    Plant.file = 'tests/test.json'
+    plant.save()
+    assert 'id' in plant.get_by_id(1)
+    assert plant.name == plant.get_by_id(1)['name']

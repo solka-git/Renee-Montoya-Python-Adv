dcd58495a6a019bd0110ac700cfe2b2aee26b297
Olha Knysh <oliaknish17@gmail.com>
2021-09-29 13:01:36 +0300

    Move save to abstract model

---
+++ homework/oop/HW_ReneeApp/framework/models.py
@@ -0,0 +1,45 @@
+"""
+базовий абстракний клас для моделей
+"""
+from abc import ABC, abstractmethod
+import json
+
+
+class Model(ABC):
+    file = "default.json"
+
+    def save(self):
+        obj_in_dict_format = self._generate_dict()
+        obj = self.get_file_data(self.file)
+        obj.append(obj_in_dict_format)
+        self.save_to_file(obj)
+
+    def _generate_dict(self):
+        return {field_name: getattr(self, field_name) for field_name in self.__dict__}
+
+
+    @classmethod
+    def get_by_id(cls, id):
+        data = cls.get_file_data(cls.file)
+        for el in data:
+            if el['id'] == id:
+                return el
+        raise Exception("Not found")
+
+    @classmethod
+    def get_all(cls):
+        data = cls.get_file_data(cls.file)
+        return data
+
+    @staticmethod
+    def get_file_data(file_name):
+        file = open("database/" + file_name, 'r')
+        data = json.loads(file.read())
+        file.close()
+        return data
+
+    def save_to_file(self, data):
+        data = json.dumps(data)
+        file = open("database/" + self.file, 'w')
+        file.write(data)
+        file.close()
---
+++ homework/oop/HW_ReneeApp/models/employee.py
@@ -0,0 +1,36 @@
+from framework.models import Model
+import json
+
+
+class Employee(Model):
+    file = "employees.json"
+
+    def __init__(self, id, name, email, department_type, department_id):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.department_type = department_type
+        self.department_id = department_id
+
+    # def __str__(self):
+    #     return f'Emp(id={self.id}, email={self.email}, name={self.name}, ' \
+    #            f'dep_type={self.department_type} dep_id={self.department_id})'
+
+
+
+    # def _generate_dict(self):
+    #     return {
+    #         'id': self.id,
+    #         'email': self.email,
+    #         'name': self.name,
+    #         'department_type': self.department_type,
+    #         'department_id': self.department_id
+    #     }
+
+    # def save(self):
+    #     employees_in_dict_format = self._generate_dict()
+    #     employees = self.get_file_data(self.file)
+    #     employees.append(employees_in_dict_format)
+    #     self.save_to_file(employees)
+
+

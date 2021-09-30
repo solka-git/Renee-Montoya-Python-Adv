32617c02fea4a8d44fdbc3448d31789108a58968
lluzhnyi <liubomyr@cozymeal.com>
2021-09-30 18:13:27 +0300

    Fix app

--- lectures/tests/ReneeMontoyaApp/test_plant.py
+++ lectures/tests/ReneeMontoyaApp/test_plant.py
@@ -1,8 +1,11 @@
-from models.plant import Plant
 from fixtures import *
-
+from models import Plant
 
 def test_plant_save(plant):
-    plant.file = 'tests/test.json'
+    file = open('database/tests/test.json', 'w')
+    file.write('[]')
+    file.close()
+    Plant.file = 'tests/test.json'
     plant.save()
     assert 'id' in plant.get_by_id(1)
+    assert plant.name == plant.get_by_id(1)['name']

6c67efebfb32a05b170436ad6383e12c4891fe63
Olha Knysh <oliaknish17@gmail.com>
2021-10-10 01:52:34 +0300

    modified test_employee.py

--- homework/tests2/ReneeMontoyaApp/test_employee.py
+++ homework/tests2/ReneeMontoyaApp/test_employee.py
@@ -1,11 +1,12 @@
 import unittest
 from unittest.mock import patch
-from models import Employee
+from models import Employee, Plant
 
 
 class TestEmployee(unittest.TestCase):
     def setUp(self):
         self.employee = Employee(1, 'Test Tester', 'test@test.com', 'plant', 1)
+        self.deparment_type = 'plant'
 
     def test_generate_dict(self):
         self.assertIn('id', self.employee._generate_dict())
@@ -17,3 +18,13 @@ class TestEmployee(unittest.TestCase):
         fileDataMock.return_value = [{"id": 1, "email": "lubomur.luzhnuy@gmail.com", "name": "Liubomyr Luzhnyi", "department_type": "plant", "department_id": 1}, {"id": 2, "email": "anton@gmail.com", "name": "Anton", "department_type": "plant", "department_id": 2}]
         self.assertEqual(self.employee.get_by_id(1)['email'], "lubomur.luzhnuy@gmail.com")
         self.assertIn('id', self.employee.get_by_id(1))
+
+    @patch('models.Plant.get_by_id')
+    def test_department(self, data_mock):
+        plant = {"id": 1, "location": "Lviv", "name": "Renee", "director_id": 1}
+        data_mock.return_value = plant
+        self.assertEqual(self.employee.department(), plant)
+        self.assertEqual(Plant.get_plant_by_director_id(1), plant)
+
+
+
